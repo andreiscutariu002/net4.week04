ef78ece1b47036a2d1df690d277510f98564b9e5
Andrei Scutariu <andrei.scutariu@endava.com>
2019-05-10 21:00:33 +0300

    add example with list

--- Week04/Week04.2.Mostenire/Program.cs
+++ Week04/Week04.2.Mostenire/Program.cs
@@ -10,21 +10,32 @@ namespace Week04._2.Mostenire
     {
         static void Main(string[] args)
         {
-            //var p = new Programmer("Andrei");
+            var p = new Programmer("Andrei");
 
-            //var m = new Manager();
-
-            //p.Metoda(2);
-
-            //m.Metoda(3);
+            var m = new Manager();
 
             var sp = new SuperProgrammer();
+
+            p.Metoda(2);
+
+            m.Metoda(3);
+
 
             sp.SetAge(22);
 
             sp.AgeProp = 17;
 
             var age = sp.GetAge();
+
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(m);
+            employees.Add(p);
+            employees.Add(sp);
+
+            foreach (var employee in employees)
+            {
+            }
         }
     }
 }
